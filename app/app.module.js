9f1ec991dfb22a6746adc58ed53935cd14e427ed
Madan <ama21673@BA92-2NF5KAG3QD.local>
2016-12-05 13:54:29 +0100

    second

--- app/app.module.js
+++ app/app.module.js
@@ -8,18 +8,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+require('./rxjs-extensions');
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
 var http_1 = require('@angular/http');
-var app_routing_module_1 = require("./app-routing.module");
-var app_component_1 = require('./app.component');
-var hero_detail_component_1 = require('./hero-detail.component');
-var heroes_component_1 = require("./heroes.component");
-var hero_service_1 = require("./hero.service");
-var dashboard_component_1 = require("./dashboard.component");
-var angular_in_memory_web_api_1 = require("angular-in-memory-web-api");
+var app_routing_module_1 = require('./app-routing.module');
+// Imports for loading & configuring the in-memory web api
+var angular_in_memory_web_api_1 = require('angular-in-memory-web-api');
 var in_memory_data_service_1 = require('./in-memory-data.service');
+var app_component_1 = require('./app.component');
+var dashboard_component_1 = require('./dashboard.component');
+var heroes_component_1 = require('./heroes.component');
+var hero_detail_component_1 = require('./hero-detail.component');
+var hero_service_1 = require('./hero.service');
+var hero_search_component_1 = require('./hero-search.component');
 var AppModule = (function () {
     function AppModule() {
     }
@@ -39,7 +42,8 @@ var AppModule = (function () {
                 app_component_1.AppComponent,
                 dashboard_component_1.DashboardComponent,
                 heroes_component_1.HeroesComponent,
-                hero_detail_component_1.HeroDetailComponent
+                hero_detail_component_1.HeroDetailComponent,
+                hero_search_component_1.HeroSearchComponent
             ],
             // services
             // this makes it a singleton for the whole application
